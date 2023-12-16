ba7c6be45ea1426053ec63b86e82c1167cb7adcc
Mohit <mohit@hey.com>
2023-12-16 11:51:25 +0530

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [mdx(), tailwind()]
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+
+			fontFamily: {
+
+				"studio_feixen_sans": ["Studio Feixen Sans", "sans"]
+
+			}
+
+		}
+	},
+	plugins: [],
+}
